a8ab3cb2052d09ca4be711bd083025271c407d41
shonsirsha <seangeekpro@gmail.com>
2022-06-19 19:56:09 +0200

    /sharing-session done

---
+++ src/pages/sharing-session/index.js
@@ -0,0 +1,112 @@
+import { useState, useEffect } from 'react'
+import formatDate from '../../../utils/formatDate'
+import { parseCookies } from '../../../utils/cookies'
+
+// ** MUI Imports
+import Grid from '@mui/material/Grid'
+import Box from '@mui/material/Box'
+import Card from '@mui/material/Card'
+import CardHeader from '@mui/material/CardHeader'
+import TextField from '@mui/material/TextField'
+import InputAdornment from '@mui/material/InputAdornment'
+
+import Magnify from 'mdi-material-ui/Magnify'
+
+import TableSharingSession from 'src/views/tables/TableSharingSession'
+
+const SharingSession = ({ registrees }) => {
+  const [filteredRegistrees, setFilteredRegistrees] = useState(registrees)
+  const [keyword, setKeyword] = useState('')
+
+  useEffect(() => {
+    if (keyword.length) {
+      let filtered = registrees.filter(
+        registree =>
+          registree.email.toLowerCase().includes(keyword) ||
+          registree.id.toString().includes(keyword) ||
+          registree.time.toString().toLowerCase().includes(keyword)
+      )
+      setFilteredRegistrees(filtered)
+    } else {
+      setFilteredRegistrees(registrees)
+    }
+  }, [keyword, setFilteredRegistrees, registrees])
+
+  return (
+    <Grid container spacing={6}>
+      <Grid item xs={12}>
+        <Card>
+          <Box
+            sx={{
+              display: 'flex',
+              width: '100%',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+              paddingRight: 4
+            }}
+          >
+            <CardHeader title='Sharing Session' titleTypographyProps={{ variant: 'h6' }} />
+            <TextField
+              onChange={e => {
+                const text = e.target.value.toLowerCase()
+                setKeyword(text)
+              }}
+              size='small'
+              placeholder='Cari...'
+              sx={{ '& .MuiOutlinedInput-root': { borderRadius: 4 } }}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position='start'>
+                    <Magnify fontSize='small' />
+                  </InputAdornment>
+                )
+              }}
+            />
+          </Box>
+
+          <TableSharingSession data={filteredRegistrees} />
+        </Card>
+      </Grid>
+    </Grid>
+  )
+}
+
+export async function getServerSideProps(ctx) {
+  const { token } = parseCookies(ctx.req)
+
+  if (!token) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: '/login'
+      }
+    }
+  }
+
+  const res = await fetch(`${process.env.NEXT_PUBLIC_REST_API_URL}/sharing-session`, {
+    method: 'GET',
+    headers: {
+      'x-auth-token': token
+    }
+  })
+
+  let registrees = (await res.json()).result
+  registrees = registrees.map(registree => ({
+    id: registree.id,
+    fullName: registree.fullName,
+    facultyDepartmentBatch: registree.facultyDepartmentBatch,
+    email: registree.email,
+    univName: registree.univName,
+    phoneNr: registree.phoneNr,
+    verif: registree.verif,
+    time: formatDate(parseInt(registree.time))
+  }))
+
+  return {
+    props: {
+      registrees
+    }
+  }
+}
+
+export default SharingSession
---
+++ src/views/tables/TableSharingSession.js
@@ -0,0 +1,98 @@
+// ** React Imports
+import { useState } from 'react'
+
+// ** MUI Imports
+import Paper from '@mui/material/Paper'
+import Table from '@mui/material/Table'
+import TableRow from '@mui/material/TableRow'
+import TableHead from '@mui/material/TableHead'
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+import TablePagination from '@mui/material/TablePagination'
+
+import formatDate from '../../../utils/formatDate'
+
+const columns = [
+  { id: 'id', label: 'ID', minWidth: 100 },
+  { id: 'fullName', label: 'Full Name', minWidth: 100 },
+  { id: 'facultyDepartmentBatch', label: 'Fac / Dep / Batch', minWidth: 100 },
+  { id: 'email', label: 'Email', minWidth: 100 },
+  { id: 'univName', label: 'University', minWidth: 100 },
+  { id: 'phoneNr', label: 'Phone', minWidth: 100 },
+  {
+    id: 'verif',
+    label: 'Verif Status',
+    minWidth: 100
+  },
+  {
+    id: 'time',
+    label: 'Tanggal & Waktu (WIB)',
+    minWidth: 100
+  }
+]
+
+const TableSharingSession = ({ data }) => {
+  // ** States
+  const [page, setPage] = useState(0)
+  const [rowsPerPage, setRowsPerPage] = useState(25)
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage)
+  }
+
+  const handleChangeRowsPerPage = event => {
+    setRowsPerPage(+event.target.value)
+    setPage(0)
+  }
+
+  return (
+    <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+      <TableContainer sx={{ maxHeight: '760px' }}>
+        <Table stickyHeader aria-label='sticky table'>
+          <TableHead>
+            <TableRow>
+              {columns.map(column => (
+                <TableCell key={column.id} align={column.align} sx={{ minWidth: column.minWidth }}>
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {[...data]
+              .reverse()
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map(row => {
+                return (
+                  <TableRow hover role='checkbox' tabIndex={-1} key={row.id}>
+                    {columns.map(column => {
+                      const value = row[column.id]
+
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          {/* {column.id === 'time' ? formatDate(parseInt(value)) : value} */}
+                          {value}
+                        </TableCell>
+                      )
+                    })}
+                  </TableRow>
+                )
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[25, 50, 100]}
+        component='div'
+        count={data.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
+  )
+}
+
+export default TableSharingSession
