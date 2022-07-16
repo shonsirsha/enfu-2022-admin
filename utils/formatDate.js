08c452d23fd7c7bc20dfe7e522abbc303cf7be40
shonsirsha <seangeekpro@gmail.com>
2022-07-16 10:58:07 +0200

    fix getMonth

--- utils/formatDate.js
+++ utils/formatDate.js
@@ -7,5 +7,5 @@ export default function formatDate(epoch) {
   const hour = zonedDate.getHours() > 9 ? zonedDate.getHours() : `0${zonedDate.getHours()}`
   const minute = zonedDate.getMinutes() > 9 ? zonedDate.getMinutes() : `0${zonedDate.getMinutes()}`
 
-  return `${zonedDate.getDate()}/${zonedDate.getMonth()}/${zonedDate.getFullYear()} - ${hour}:${minute}`
+  return `${zonedDate.getDate()}/${zonedDate.getMonth() + 1}/${zonedDate.getFullYear()} - ${hour}:${minute}`
 }
